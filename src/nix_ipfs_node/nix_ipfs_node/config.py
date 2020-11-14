1c52cd0bdd423837758f78da956e2be80cd6e714
Kevin Amado <kamadorueda@gmail.com>
2020-11-13 22:55:08 -0500

    add helper functions

--- src/nix_ipfs_node/nix_ipfs_node/config.py
+++ src/nix_ipfs_node/nix_ipfs_node/config.py
@@ -13,3 +13,22 @@ _SUBSTITUTER = urllib.parse.urlparse(os.environ['NIX_IPFS_NODE_SUBSTITUTER'])
 SUBSTITUTER_SCHEME = _SUBSTITUTER.scheme
 SUBSTITUTER_NETLOC = _SUBSTITUTER.netloc
 SUBSTITUTER = f'{SUBSTITUTER_SCHEME}://{SUBSTITUTER_NETLOC}'
+
+
+def build_coordinator_url(path: str, **kwargs: str) -> str:
+    path = path.format(**{
+        key: urllib.parse.quote_plus(val) for key, val in kwargs.items()
+    })
+
+    return f'{COORDINATOR_URL}/{path}'
+
+
+def build_substituter_url(path: str) -> str:
+    return f'{SUBSTITUTER}/{path}'
+
+
+def patch_substituter_headers(headers: Headers) -> Dict[str, str]:
+    headers_dict: Dict[str, str] = dict(headers)
+    headers_dict['host'] = SUBSTITUTER_NETLOC
+
+    return headers_dict
