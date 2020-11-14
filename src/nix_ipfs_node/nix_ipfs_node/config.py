68ef6495540787074dd2d65401324d04ba30e8cc
Kevin Amado <kamadorueda@gmail.com>
2020-11-13 22:23:04 -0500

    protect from attack vector

--- src/nix_ipfs_node/nix_ipfs_node/config.py
+++ src/nix_ipfs_node/nix_ipfs_node/config.py
@@ -2,9 +2,12 @@
 import os
 import urllib.parse
 
-# Constants
+# Environment
 COORDINATOR_URL = os.environ['NIX_IPFS_COORDINATOR_URL']
 PORT = os.environ['NIX_IPFS_NODE_PORT']
+
+# Constants
+MAX_AGGRESSIVE_READ = 1048576  # 1 MiB
 
 _SUBSTITUTER = urllib.parse.urlparse(os.environ['NIX_IPFS_NODE_SUBSTITUTER'])
 SUBSTITUTER_SCHEME = _SUBSTITUTER.scheme
--- src/nix_ipfs_node/nix_ipfs_node/http.py
+++ src/nix_ipfs_node/nix_ipfs_node/http.py
@@ -2,8 +2,16 @@
 import aiohttp
 import contextlib
 from typing import (
+    AsyncIterable,
     Optional,
-    NamedTuple,
+)
+
+# Third party libraries
+from starlette.datastructures import (
+    Headers,
+)
+from starlette.responses import (
+    StreamingResponse,
 )
 
 @contextlib.asynccontextmanager
@@ -12,7 +20,7 @@ async def request(
     headers: Optional[dict] = None,
     method: str,
     url: str,
-):
+) -> aiohttp.ClientResponse:
     connector = aiohttp.TCPConnector(verify_ssl=False)
     timeout = aiohttp.ClientTimeout(connect=None, total=60, sock_connect=None, sock_read=None)
 
@@ -29,9 +37,40 @@ async def request(
             yield response
 
 
-async def stream_response(response: aiohttp.ClientResponse):
+async def stream_response(
+    *,
+    chunk_size: int = 1024,
+    response: aiohttp.ClientResponse,
+) -> AsyncIterable[bytes]:
     while True:
-        if chunk := await response.content.read(1024):
+        if chunk := await response.content.read(chunk_size):
             yield chunk
         else:
             break
+
+
+async def stream_from_substituter(
+    headers: Headers,
+    method: str,
+    url: str,
+) -> StreamingResponse:
+
+    async def generate_content():
+        async with request(
+            headers=headers,
+            method=method,
+            url=url,
+        ) as response:
+            yield response.status
+
+            async for chunk in stream_response(response=response):
+                yield chunk
+
+    content_generator = generate_content()
+    status_code = await content_generator.asend(None)
+
+    return StreamingResponse(
+        content=content_generator,
+        media_type='application/octet-stream',
+        status_code=status_code,
+    )
