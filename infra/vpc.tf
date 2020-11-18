3c847273f56df9ecd4d85ead95c05c099b8ce76d
Kevin Amado <kamadorueda@gmail.com>
2020-11-17 21:11:49 -0500

    feat(infra): subnet and route tables

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,3 +1,26 @@
+resource "aws_route_table" "public" {
+  tags = {
+    "Name" = "4shells"
+    "management:product" = "4shells"
+  }
+  vpc_id = aws_vpc.four_shells.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public_1.id
+}
+
+resource "aws_subnet" "public_1" {
+  availability_zone = "${var.region}a"
+  cidr_block = "192.168.0.0/24"
+  tags = {
+    "Name" = "4shells"
+    "management:product" = "4shells"
+  }
+  vpc_id = aws_vpc.four_shells.id
+}
+
 resource "aws_vpc" "four_shells" {
   assign_generated_ipv6_cidr_block = false
   cidr_block = "192.168.0.0/16"
