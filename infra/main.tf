4eddee57645f3418b83d336daa9c27ed6a7366b6
Kevin Amado <kamadorueda@gmail.com>
2020-11-17 20:58:16 -0500

    feat(infra): region variable

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = "us-east-1"
+  region = var.region
 }
 
 terraform {
--- infra/vars.tf
+++ infra/vars.tf
@@ -1,3 +1,6 @@
 data "aws_caller_identity" "current" {}
 variable "access_key" {}
+variable "region" {
+  default = "us-east-1"
+}
 variable "secret_key" {}
