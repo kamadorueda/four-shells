a5e434b5acc5cd1c26f0f9509f71fa2482b4b720
Kevin Amado <kamadorueda@gmail.com>
2020-11-20 19:50:43 -0500

    feat(build): adjust docker image

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-# syntax=docker/dockerfile:experimental
-
 # Base alpine image configured with Nix
 FROM nixos/nix:2.3
 
+# Metadata
+LABEL 'org.opencontainers.image.source' 'https://github.com/kamadorueda/4shells.com'
+
+# Constants
 ENV FOUR_SHELLS_REPO https://github.com/kamadorueda/4shells.com
 ENV FOUR_SHELLS_REV c62f34fee27217956248451005b7710e52313f5d
 
@@ -12,8 +14,7 @@ ENV NIX_IPFS_REV aa9b05cf18498c4020a607831876fc5337cd1bfc
 # Install all products from their respective sources
 RUN   true \
   &&  nix-env -i -f "${FOUR_SHELLS_REPO}/archive/${FOUR_SHELLS_REV}.tar.gz" \
-  &&  nix-env -i -f "${NIX_IPFS_REPO}/archive/${NIX_IPFS_REV}.tar.gz" \
   &&  nix-store --optimise
 
-# Metadata
-LABEL 'org.opencontainers.image.source' 'https://github.com/kamadorueda/4shells.com'
+# Interpreter for commands
+ENTRYPOINT ["sh", "-c"]
--- build/image-deploy.sh
+++ build/image-deploy.sh
@@ -9,22 +9,21 @@
 source "${srcBuildCtxSh}"
 
 function main {
-    export AWS_ACCESS_KEY_ID="${ADMIN_AWS_ACCESS_KEY_ID}"
-    export AWS_SECRET_ACCESS_KEY="${ADMIN_AWS_SECRET_ACCESS_KEY}"
-    local account_id='791877604510'
-    local region='us-east-1'
-    local registry="${account_id}.dkr.ecr.${region}.amazonaws.com"
-    local repository='four_shells'
-    local target="${registry}/${repository}:latest"
+  export AWS_ACCESS_KEY_ID="${ADMIN_AWS_ACCESS_KEY_ID}"
+  export AWS_SECRET_ACCESS_KEY="${ADMIN_AWS_SECRET_ACCESS_KEY}"
+  local account_id='791877604510'
+  local region='us-east-1'
+  local registry="${account_id}.dkr.ecr.${region}.amazonaws.com"
+  local repository='four_shells'
+  local target="${registry}/${repository}:latest"
 
-        echo '[INFO] Building' \
-    &&  DOCKER_BUILDKIT=1 \
-        docker build --tag "${target}" . \
-    &&  echo '[INFO] Authenticating' \
-    &&  aws ecr get-login-password --region "${region}" \
-          | docker login --username AWS --password-stdin "${registry}" \
-    &&  echo '[INFO] Pushing' \
-    &&  docker push "${target}"
+      echo "[INFO] Building: ${target}" \
+  &&  docker build --file 'Dockerfile' --tag "${target}" "$(mktemp -d)" \
+  &&  echo "[INFO] Authenticating to: ${registry}" \
+  &&  aws ecr get-login-password --region "${region}" \
+        | docker login --username AWS --password-stdin "${registry}" \
+  &&  echo "[INFO] Pushing: ${target}" \
+  &&  docker push "${target}"
 }
 
 main "${@}"
