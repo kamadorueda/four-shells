91a12eabd2253fa94073f0b36214f4626a40adb9
Kevin Amado <kamadorueda@gmail.com>
2020-11-22 15:23:16 -0500

    feat(back): add low level access to db

--- server/pkgs/cachipfs/cachipfs/persistence/db.py
+++ server/pkgs/cachipfs/cachipfs/persistence/db.py
@@ -1,5 +1,20 @@
+# Standard library
+from enum import (
+    Enum,
+)
+from typing import (
+    Any,
+)
+
 # Third party libraries
+from aioextensions import (
+    in_thread,
+)
 import boto3
+from boto3.resources.factory.dynamodb import (
+    ServiceResource as ServiceResourceType,
+    Table as TableType,
+)
 import botocore.config
 
 # Local libraries
@@ -8,7 +23,7 @@ from cachipfs import (
 )
 
 # Constants
-RESOURCE = boto3.resource(
+RESOURCE: ServiceResourceType = boto3.resource(
     aws_access_key_id=config.AWS_ACCESS_KEY_ID_SERVER,
     aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY_SERVER,
     config=botocore.config.Config(
@@ -19,3 +34,39 @@ RESOURCE = boto3.resource(
     service_name='dynamodb',
     verify=False,
 )
+
+
+class TableEnum(Enum):
+    accounts: str = 'cachipfs_accounts'
+    namespaces: str = 'cachipfs_namespaces'
+    objects: str = 'cachipfs_objects'
+
+
+# References:
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Table.get_item
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Table.put_item
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Table.delete_item
+
+
+async def get(table: TableEnum, *args: Any, **kwargs: Any) -> Any:
+    table_resource: TableType = RESOURCE.Table(table.value)
+
+    response = await in_thread(table_resource.get_item, *args, **kwargs)
+
+    return response
+
+
+async def put(table: TableEnum, *args: Any, **kwargs: Any) -> Any:
+    table_resource: TableType = RESOURCE.Table(table.value)
+
+    response = await in_thread(table_resource.put_item, *args, **kwargs)
+
+    return response
+
+
+async def delete(table: TableEnum, *args: Any, **kwargs: Any) -> Any:
+    table_resource: TableType = RESOURCE.Table(table.value)
+
+    response = await in_thread(table_resource.delete_item, *args, **kwargs)
+
+    return response
