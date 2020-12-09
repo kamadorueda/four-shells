a4ea63cec90a95d02a0eff77a459a132cfd67021
Kevin Amado <kamadorueda@gmail.com>
2020-12-08 23:07:45 -0500

    feat(front): add badges

--- server/front/src/components/nixdb/Pkg.jsx
+++ server/front/src/components/nixdb/Pkg.jsx
@@ -1,12 +1,84 @@
 // Third party libraries
-import { Card, CardContent, Grid, Typography } from '@material-ui/core';
-import React from 'react';
+import {
+  Card,
+  CardContent,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { Code } from '../Code';
 
 // Local libraries
 import { Progress } from '../Progress';
 import { useFetchJSON } from './utils';
+
+// Constants
+const FORMATS_FUNCTIONS = {
+  "AsciiDoc": (imageURL, linkURL) => `
+    image:${imageURL}[link="${linkURL}"]
+  `,
+  "HTML": (imageURL, linkURL) => `
+    <a href="${linkURL}">
+      <img src="${imageURL}">
+    </a>
+  `,
+  "Markdown": (imageURL, linkURL) => `
+    [![](${imageURL})](${linkURL})
+  `,
+  "ReStructuredText": (imageURL, linkURL) => `
+    .. image:: ${imageURL}
+      :target: ${linkURL}
+  `,
+};
+const FORMATS = Object.keys(FORMATS_FUNCTIONS);
+export const COLORS = [
+  "blue",
+  "blueviolet",
+  "brightgreen",
+  "green",
+  "lightgrey",
+  "orange",
+  "red",
+  "yellow",
+  "yellowgreen",
+];
+
+export const STYLES = [
+  "flat",
+  "flat-square",
+  "plastic",
+  "for-the-badge",
+];
+
+export const badge = ({
+  color = "green",
+  label,
+  labelColor = "grey",
+  logo = "NixOS",
+  logoColor = "white",
+  pkg,
+  style = "flat",
+}) => {
+  const url = new URL("https://img.shields.io/endpoint");
+
+  url.searchParams.set("color", color);
+  url.searchParams.set("label", label === undefined ? pkg : label);
+  url.searchParams.set("labelColor", labelColor);
+  url.searchParams.set("logo", logo);
+  url.searchParams.set("logoColor", logoColor);
+  url.searchParams.set("style", style);
+  url.searchParams.set("url", `https://raw.githubusercontent.com/kamadorueda/nixpkgs-db/latest/data/badges/${pkg}.json`);
+
+  return url.toString();
+}
 
 const Definition = ({
   title,
@@ -65,6 +137,95 @@ const formatPlatforms = (platforms) => {
   }
 
   return formatted.sort();
+}
+
+const Badge = ({ pkg }) => {
+  const [badgeId, setBadgeId] = useState('flat/blue');
+  const [badgesData, setBadgesData] = useState([]);
+  const [content, setContent] = useState("");
+  const [format, setFormat] = useState(FORMATS[2]);
+  const [label, setLabel] = useState(pkg);
+
+  const linkURL = `https://4shells.com/nixdb/pkg/${encodeURIComponent(pkg)}`;
+
+  useEffect(() => {
+    const newBadgesData = [].concat.apply([],
+      STYLES.map((style) => COLORS.map((color) => {
+        const imageURL = badge({ color, label, pkg, style });
+        const badgeContent = FORMATS_FUNCTIONS[format](imageURL, linkURL);
+
+        return {
+          badgeContent,
+          color,
+          imageURL,
+          style,
+        };
+      }))
+    );
+
+    setBadgesData(newBadgesData);
+    setContent(newBadgesData[0].badgeContent);
+  }, [format, label, linkURL, pkg])
+
+  const onBadgeSelection = (content) => (event) => {
+    setContent(content);
+    setBadgeId(event.target.value);
+  };
+  const onChangeFormat = (event) => {
+    setFormat(event.target.value);
+  };
+  const onChangeLabel = (event) => {
+    setLabel(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <DefinitionText content='Add the badge of your preference to your project!'/>
+      <DefinitionText content='It will tell your users the number of releases they can install with Nix and link to this page so they can get more information!'/>
+      <br />
+      <FormControl variant="outlined">
+        <InputLabel>Format</InputLabel>
+        <Select
+          value={format}
+          onChange={onChangeFormat}
+          label="Format"
+        >
+          {FORMATS.map((f) => <MenuItem key={f} value={f}>{f}</MenuItem>)}
+        </Select>
+        <br />
+        <TextField
+          defaultValue={pkg}
+          label="Custom label"
+          onChange={onChangeLabel}
+          value={label}
+          variant='outlined'
+        />
+      </FormControl>
+      <br />
+      <br />
+      <Grid container>
+        <RadioGroup row>
+          {badgesData.map(({ badgeContent, color, imageURL, style }) => {
+            const radioID = `${style}/${color}`;
+
+            return (
+              <Grid item xs={12} sm={6} md={4}>
+                <Radio
+                  checked={radioID == badgeId}
+                  onChange={onBadgeSelection(badgeContent)}
+                  size='small'
+                  value={radioID}
+                />
+                <img alt={radioID} src={imageURL}/>
+              </Grid>
+            );
+          })}
+        </RadioGroup>
+      </Grid>
+      <br />
+      <Code content={content} copyable={true} />
+    </React.Fragment>
+  );
 }
 
 export const Pkg = () => {
@@ -160,13 +321,13 @@ export const Pkg = () => {
       <Definition title='Use in an expression'>
         <Code content={nixBuild} copyable={true} lang='nix' />
       </Definition>
+      <Definition title='Badges'>
+        <Badge pkg={pkg} />
+      </Definition>
       <Definition title='Commits range'>
         <DefinitionText content={
           `${versionData?.revs[0]} -> ${versionData?.revs[1]}`
         } />
-      </Definition>
-      <Definition title='Raw data'>
-        <DefinitionText content={versionData?.meta?.license?.fullName}/>
       </Definition>
       <Definition title='Available platforms'>
         <Grid container>
