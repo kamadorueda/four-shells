d11b2385cbad317e5cf44361bb056b68d150f80b
Kevin Amado <kamadorueda@gmail.com>
2020-12-11 22:39:58 -0500

    cachipfs\feat(client): add config

---
+++ client/four_shells/config.py
@@ -0,0 +1,10 @@
+# Constants
+COORDINATOR_URL = 'https://4shells.com'
+DATA: str = ''
+DATA_CACHIPFS: str = ''
+DATA_CACHIPFS_REPO: str = ''
+DATA_EPHEMERAL: str = ''
+CACHIPFS_API_PORT: int = 0
+CACHIPFS_GATEWAY_PORT: int = 0
+CACHIPFS_NODE_PORT: int = 0
+CACHIPFS_SWARM_PORT: int = 0
